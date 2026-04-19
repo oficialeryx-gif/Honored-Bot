e7ac41abb2a5514df6911ebdba10d5995e0fa072
oficialeryx-gif <oficialeryx@gmail.com>
2026-04-19 19:43:51 +0200

    Delete Honored-Bot directory

--- Honored-Bot/src/events/ready.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'ready',
-  once: true,
-  async execute(client) {
-    console.log(`✅ ${client.user.tag} est en ligne !`);
-    client.user.setActivity('le serveur Honored 👑', { type: 3 });
-  },
-};
